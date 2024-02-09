b8a0538237a238c9c00c96a7c36a2ea1341b6e43
M0tionl3ss <denauwjonas@gmail.com>
2024-02-09 13:25:52 +0100

    First commit

---
+++ init.lua
@@ -0,0 +1,23 @@
+-- UI
+vim.opt.relativenumber = true
+vim.opt.number = true
+vim.opt.title = true
+
+-- Text options
+vim.opt.formatoptions = {j = true}
+vim.opt.autoindent= true
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.smarttab = true
+vim.opt.tabstop = 4
+
+-- Mappings
+vim.keymap.set('i', 'kj', '<ESC>')
+vim.keymap.set('i', '<C-l>', '<ESC>')
+
+-- Macro
+vim.opt.lazyredraw = true
+
+
+
+
