9820cb2a1d4dc818048694477afdb631ad735c5c
M0tionl3ss <denauwjonas@gmail.com>
2024-02-15 21:03:45 +0100

    netrw

--- init.lua
+++ init.lua
@@ -5,6 +5,15 @@ require('lsp')
 vim.opt.relativenumber = true
 vim.opt.number = true
 vim.opt.title = true
+vim.g.netrw_winsize = 30
+
+-- NETRW
+
+vim.g.netrw_liststyle=3
+vim.g.netrw_hide = 1
+vim.g.netrw_browse_split = 4
+
+vim.api.nvim_create_autocmd({"VimEnter"}, {command = 'Vexplore' })
 
 -- Text options
 vim.opt.formatoptions = {j = true}
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -12,4 +12,10 @@ return require('packer').startup(function(use)
     use 'saadparwaiz1/cmp_luasnip'
     use 'L3MON4D3/LuaSnip' 
 
+    -- Icons
+    use 'nvim-tree/nvim-web-devicons'
+
+    -- File Explorer
+    -- use { 'nvim-neo-tree/neo-tree.nvim', branch = "v3.x", requires = { 'nvim-lua/plenary.nvim', 'MunifTanjim/nui.nvim'} }
+
 end)
